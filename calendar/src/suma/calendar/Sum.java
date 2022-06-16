48fb9fe3c789e66ebdc14758ba2216eded34bdd5
SJ Jeon <ayclub8866@naver.com>
2022-06-17 00:14:58 +0900

    input/output calendar

---
+++ calendar/src/suma/calendar/Sum.java
@@ -0,0 +1,26 @@
+package suma.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		//입력: 키보드로 두 수의 입력을 받는다.
+		Scanner sc = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해 주세요");
+		s1 = sc.next();
+		s2 = sc.next();
+		System.out.println(s1 + " " + s2);
+		num1 = Integer.parseInt(s1);
+		num2 = Integer.parseInt(s2);
+
+		//출력: 화면에 두 수의 합을 출력한다.
+	    //System.out.println("두 수의 합은 " + (num1 + num2) + "입니다.");
+		System.out.printf("%d와 %d의 합은 %d입니다", num1 , num2, num1 + num2);
+		sc.close();
+	}
+
+}
